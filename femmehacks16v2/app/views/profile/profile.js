c9d697c5d41a46a9229d8e4e33a12c46bfe9c8d2
Lily Romano <kanntara@gmail.com>
2016-02-20 14:45:06 -0500

    update of profile.  show/hide js working

---
+++ femmehacks16v2/app/views/profile/profile.js
@@ -0,0 +1,33 @@
+var maxheight = 80;
+var showText = "See More>>>";
+var hideText = "Hide<<<";
+
+$(document).ready(function() {
+
+
+  $('.expandable').each(function() {
+    var text = $(this);
+    if (text.height() > maxheight) {
+      text.css({
+        'overflow': 'hidden',
+        'height': maxheight + 'px'
+      });
+
+      var link = $('<div style="position:relative;" class="container-fluid showhide"><a href="#" class="showhide">' + showText + '</a></div>');
+      var linkDiv = $('<div class="test"></div>');
+      linkDiv.append(link);
+      $(this).after(linkDiv);
+
+      link.click(function(event) {
+        event.preventDefault();
+        if (text.height() > maxheight) {
+          $(this).html(showText);
+          text.css('height', maxheight + 'px');
+        } else {
+          $(this).html(hideText);
+          text.css('height', 'auto');
+        }
+      });
+    }
+  });
+});
